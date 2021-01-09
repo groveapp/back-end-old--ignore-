c99bf7487cd963a2ac725c78f7e63471909abdd4
Vitoria Postai Martins <49215543+vitoriapostaimartins@users.noreply.github.com>
2021-01-09 15:07:14 -0800

    added test queries

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require('./queries')
 let app = express();
 
 app.use(express.urlencoded({extended: true}));
@@ -13,6 +14,12 @@ data = {
   };
 
 app.get("/", (req, res)=> res.json(data));
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 
 app.listen(3000);
