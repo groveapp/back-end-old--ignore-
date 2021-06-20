c34c6bb963eebf9f0bb28e2a31591fbb400f5c79
amanjpatel <amanpatel1777@gmail.com>
2021-06-20 17:20:13 -0400

    new notes

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,13 +1,12 @@
 const { Router } = require('express');
 
-//notes: implemented lazy loading for association requests (like getUserByOrganization).
+// notes: implemented lazy loading for association requests (like getUserByOrganization).
 // This implies that if we want to know all the data about all the users in an organization,
 // we have to first give a frontend query to get all user ids, and then process another frontend query
 // to get all the info for all the user ids. Basically, the new routing is done on the frontend.
 
 // implement leader.json
 // implement POST for association tables
-
 
 // not being used:
 
@@ -17,17 +16,17 @@ const { Router } = require('express');
     // router.put('/test/:testId', controllers.test.updateTest)
     // router.delete('/test/:testId', controllers.test.deleteTest)
 
-// router.get('/leader/action/:leaderId', controllers.leader.getLeaderActionByLeader)
+    // router.get('/leader/action/:leaderId', controllers.leader.getLeaderActionByLeader)
 
-// router.get('/replies/:userId', controllers.replies.getRepliesByUser)
-// router.get('/replies/:viewpointId', controllers.replies.getRepliesByViewpoint)
+    // router.get('/replies/:userId', controllers.replies.getRepliesByUser)
+    // router.get('/replies/:viewpointId', controllers.replies.getRepliesByViewpoint)
 
-// router.get('/tags/:issueId', controllers.tag.getTagsByIssue)
-// router.get('/issues/:tagId', controllers.issue.getIssuesByTag)
-// router.post('/issues/:issueTagObj', controllers.issue.addTag)
-// router.delete('/issues/:issueTagObj', controllers.issue.deleteTag)
+    // router.get('/tags/:issueId', controllers.tag.getTagsByIssue)
+    // router.get('/issues/:tagId', controllers.issue.getIssuesByTag)
+    // router.post('/issues/:issueTagObj', controllers.issue.addTag)
+    // router.delete('/issues/:issueTagObj', controllers.issue.deleteTag)
 
-// router.get('/replies/:replyId', controllers.reply.getReplyById)
+    // router.get('/replies/:replyId', controllers.reply.getReplyById)
     // router.post('/replies/:replyObj', controllers.reply.createReply)
     // router.put('/replies/:replyObj', controllers.reply.updateReply)
     // router.delete('/replies/:replyId', controllers.reply.deleteReply)
@@ -43,19 +42,18 @@ module.exports = function(server,controllers) {
 
     //done
 
+    //for post and update: make sure associations can be posted/updated too
+
     router.get('/user/:userId', controllers.user.getUserById)
     router.post('/user', controllers.user.createUser)
     router.put('/user/:userId', controllers.user.updateUser)
     router.delete('/user/:userId', controllers.user.deleteUser)
     router.get('/user/organization/:organizationId', controllers.user.getUserIdByOrganization)
 
-
-
     router.get('/profile/:profileId', controllers.profile.getProfileById)
     router.post('/profile', controllers.profile.createProfile)
     router.put('/profile/:profileId', controllers.profile.updateProfile)
     router.delete('/profile/:profileId', controllers.profile.deleteProfile)
-    //unneeded: router.get('/profile/user/:userId', controllers.profile.getProfileByUser)
 
     router.get('/leader/:leaderId', controllers.leader.getLeaderById)
     router.post('/leader', controllers.leader.createLeader)
@@ -83,7 +81,6 @@ module.exports = function(server,controllers) {
     //router.get('/viewpoint/issue/:issueId', controllers.viewpoint.getViewpointByIssue)
     //router.get('/viewpoint/user/:userId', controllers.viewpoint.getViewpointByUser)
 
-    
     router.get('/rawVote/:voteId', controllers.rawVote.getRawVoteById)
     router.post('/rawVote', controllers.rawVote.createRawVote)
     router.put('/rawVote', controllers.rawVote.createRawVote)
